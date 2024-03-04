d53d01e7953e9b1a386923b48423fe2576509398
Damien Bezborodov <dbezborodov@gmail.com>
2024-03-04 18:04:14 +1030

    Test for multidim.

--- tests/test_form_data.py
+++ tests/test_form_data.py
@@ -13,8 +13,33 @@ class TestFormData(unittest.TestCase):
         fields = form.fields({'project_id', 'account_id', 'project_code'})
         self.assertEqual(data, fields)
 
+        # Test that only acceptable fields are returned.
         fields = form.fields({'project_id'})
         self.assertEqual({'project_id': '5'}, fields)
+
+
+    def test_multidimensional(self):
+        data = {
+            'project_id[0]': '52',
+            'account_id[0]': '30',
+            'project_id[1]': '45',
+            'account_id[1]': '20',
+            'group_id': '500',
+            'ignored_field': 'ignore_me',
+        }
+        form = FormData(data)
+        fields = form.fields({'project_id', 'account_id', 'group_id'})
+        self.assertEqual({
+            'project_id': {
+                0: '52',
+                1: '45',
+            },
+            'account_id': {
+                0: '30',
+                1: '20',
+            },
+            'group_id': '500',
+        }, fields)
 
 
 if __name__ == '__main__':
